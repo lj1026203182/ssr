9cd67a0cfe8687cf8624f17645033d2961840a53
lj1026203182 <lj1026203182@qq.com>
2020-04-10 16:56:33 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import { createRouter } from "./router";
+import { createStore } from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+export function createApp() {
+  // 创建 router 实例
+  const router = createRouter();
+  const store = createStore();
+
+  const app = new Vue({
+    router,
+    store,
+    render: h => h(App)
+  });
+  return { app, router, store };
+}
